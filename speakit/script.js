6780049c8ffae0d129308e7885e2933c7f2093db
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-06-15 23:45:00 +0300

    init: start speakit task

---
+++ speakit/script.js
@@ -0,0 +1,8 @@
+console.log('Hello');
+const getWords = async (page, group) => {
+  const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
+  const res = await fetch(url);
+  const json = await res.json();
+  console.log(JSON.stringify(json, null, 1));
+};
+getWords(1, 1);
