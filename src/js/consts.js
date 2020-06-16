6cefd5997f832b016cb0d2d72931e492c2bc7508
leqsar <47753538+leqsar@users.noreply.github.com>
2020-06-16 17:46:36 +0300

    feat: add animantion for main word and add generation of all elements on the game page

---
+++ src/js/consts.js
@@ -0,0 +1,35 @@
+const words = [
+  {
+    word: 'Cat',
+    translation: 'Кот'
+  },
+  {
+    word: 'Dog',
+    translation: 'Кот'
+  },
+  {
+    word: 'Pig',
+    translation: 'Свинья'
+  },
+  {
+    word: 'Mom',
+    translation: 'Мать'
+  },
+  {
+    word: 'Dad',
+    translation: 'Отец'
+  },
+  {
+    word: 'Brother',
+    translation: 'Брат'
+  },
+  {
+    word: 'Spoon',
+    translation: 'Ложка'
+  },
+  {
+    word: 'Khife',
+    translation: 'Нож'
+  },
+]
+export default words;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import words from './consts'
+
 window.onload = () => {
   const startingButton = document.querySelector('.starting-button');
   const infoContainer = document.querySelector('.info');
@@ -7,6 +9,7 @@ window.onload = () => {
   const volumeContainer = document.querySelector('.volume-settings');
   const volumeOn = document.querySelector('.volume-up-icon');
   const volumeOff = document.querySelector('.volume-mute-icon');
+
   startingButton.addEventListener('click', () => {
     const loader = document.createElement('DIV');
     loader.classList.add('loader');
@@ -20,11 +23,46 @@ window.onload = () => {
         resolve();
       }, 4000);
     })
+    async function fetchWords() {
+      const answers = await words;
+      return answers;
+    }
     showingDetails.then(() => {
+      const arrayWithWords = fetchWords();
+      console.log(arrayWithWords);
       healthPoints.style.visibility = 'visible';
       volumeContainer.style.visibility = 'visible';
+      const answersContainer = document.createElement('DIV');
+      const firstAnswer = document.createElement('P');
+      const secondAnswer = document.createElement('P');
+      const thirdAnswer = document.createElement('P');
+      const forthAnswer = document.createElement('P');
+      const mainWord = document.createElement('P');
+      answersContainer.append(firstAnswer);
+      answersContainer.append(secondAnswer);
+      answersContainer.append(thirdAnswer);
+      answersContainer.append(forthAnswer);
+      answersContainer.classList.add('answers');
+      mainWord.classList.add('main-word');
+      main.append(answersContainer);
+      main.prepend(mainWord);
+      const start = Date.now();
+      const timer = setInterval(() => {
+        const timePassed = Date.now() - start;
+        if (timePassed >= 5000) {
+          clearInterval(timer);
+          return;
+        }
+        function draw(time) {
+          mainWord.style.top = `${time / 10}px`;
+        }
+        draw(timePassed);
+      }, 20);
     })
   });
+
+
+
   volumeContainer.addEventListener('click', () => {
     if (volumeOn.style.display === 'none') {
       volumeOn.style.display = 'block';
