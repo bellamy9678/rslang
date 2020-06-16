e31e214ca20ee7288eb5091ed323571d40ea2bd9
leqsar <47753538+leqsar@users.noreply.github.com>
2020-06-16 16:37:25 +0300

    feat: add changes of volume icon in silent and standart mode

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,35 @@ window.onload = () => {
   const infoContainer = document.querySelector('.info');
   const main = document.querySelector('MAIN');
   const hint = document.querySelector('.hint');
+  const healthPoints = document.querySelector('.health-point-scale');
+  const volumeContainer = document.querySelector('.volume-settings');
+  const volumeOn = document.querySelector('.volume-up-icon');
+  const volumeOff = document.querySelector('.volume-mute-icon');
   startingButton.addEventListener('click', () => {
     const loader = document.createElement('DIV');
     loader.classList.add('loader');
     infoContainer.remove();
     hint.style.visibility = 'visible';
     main.prepend(loader);
-    setTimeout(() => {
-      loader.remove();
-      hint.style.visibility = 'hidden';
-    }, 4000)
+    const showingDetails = new Promise ((resolve) => {
+      setTimeout(() => {
+        loader.remove();
+        hint.style.visibility = 'hidden';
+        resolve();
+      }, 4000);
+    })
+    showingDetails.then(() => {
+      healthPoints.style.visibility = 'visible';
+      volumeContainer.style.visibility = 'visible';
+    })
   });
+  volumeContainer.addEventListener('click', () => {
+    if (volumeOn.style.display === 'none') {
+      volumeOn.style.display = 'block';
+      volumeOff.style.display = 'none';
+    } else {
+      volumeOn.style.display = 'none';
+      volumeOff.style.display = 'block';
+    }
+  })
 }
