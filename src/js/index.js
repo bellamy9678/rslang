0c52f841b347e932d13835fc1d70a1586d293d1a
leqsar <47753538+leqsar@users.noreply.github.com>
2020-06-16 14:14:45 +0300

    feat: add representation of bindings

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 window.onload = () => {
-
+  const startingButton = document.querySelector('.starting-button');
+  const infoContainer = document.querySelector('.info');
+  const main = document.querySelector('MAIN');
+  const hint = document.querySelector('.hint');
+  startingButton.addEventListener('click', () => {
+    const loader = document.createElement('DIV');
+    loader.classList.add('loader');
+    infoContainer.remove();
+    hint.style.visibility = 'visible';
+    main.prepend(loader);
+    setTimeout(() => {
+      loader.remove();
+      hint.style.visibility = 'hidden';
+    }, 4000)
+  });
 }
