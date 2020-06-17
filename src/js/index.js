55a3eadc5f5ac4d4dbe730400fb813cfe86e9b23
leqsar <47753538+leqsar@users.noreply.github.com>
2020-06-17 14:33:10 +0300

    feat: implement function of random element generation

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,14 @@ window.onload = () => {
   const volumeContainer = document.querySelector('.volume-settings');
   const volumeOn = document.querySelector('.volume-up-icon');
   const volumeOff = document.querySelector('.volume-mute-icon');
+  const stone = document.querySelector('.stone-icon');
+  const arrayWithWords = [];
+
+  function arrayRandElement(fullArray) {
+    const randomIndex = Math.floor(Math.random() * fullArray.length);
+    arrayWithWords.push(fullArray[randomIndex]);
+    fullArray.splice(randomIndex, 1);
+  }
 
   startingButton.addEventListener('click', () => {
     const loader = document.createElement('DIV');
@@ -24,14 +32,24 @@ window.onload = () => {
       }, 4000);
     })
     async function fetchWords() {
-      const answers = await words;
-      return answers;
+      return new Promise((resolve) => {
+        resolve(words);
+      })
     }
+
     showingDetails.then(() => {
-      const arrayWithWords = fetchWords();
-      console.log(arrayWithWords);
+      fetchWords().then(
+        (resolve) => {
+          const wordInfo = resolve;
+          for (let i = 0; i < 3; i += 1) {
+            arrayRandElement(wordInfo);
+          }
+          console.log(arrayWithWords);
+        }
+      )
       healthPoints.style.visibility = 'visible';
       volumeContainer.style.visibility = 'visible';
+      stone.style.visibility = 'visible';
       const answersContainer = document.createElement('DIV');
       const firstAnswer = document.createElement('P');
       const secondAnswer = document.createElement('P');
@@ -46,6 +64,7 @@ window.onload = () => {
       mainWord.classList.add('main-word');
       main.append(answersContainer);
       main.prepend(mainWord);
+
       const start = Date.now();
       const timer = setInterval(() => {
         const timePassed = Date.now() - start;
