4d19d2a0548d17d3bb64c7a75a1541b75b3d2019
Michael Fursa <bellamy9678@yahoo.com>
2020-06-18 15:43:42 +0300

    feat: add events names

---
+++ src/observer/events.js
@@ -0,0 +1,17 @@
+export const eventPushedAgain = 'againWordButtonPushed';
+export const eventPushedEasy = 'easyWordButtonPushed';
+export const eventPushedGood = 'goodWordButtonPushed';
+export const eventPushedHard = 'hardWordButtonPushed';
+export const eventPushedRemoveFromDictionary = 'deleteFromDictionaryButtonPushed';
+export const eventPushedAddToDifficult = 'addToDifficultButtonPushed';
+export const eventPushedShowAnswerButton = 'showAnswerButtonPushed';
+
+export const customEvents = {
+  pushedAgain: "againWordButtonPushed",
+  pushedEasy: "easyWordButtonPushed",
+  pushedGood: "goodWordButtonPushed",
+  pushedHard: "hardWordButtonPushed",
+  pushedRemoveFromDictionary: "deleteFromDictionaryButtonPushed",
+  pushedAddToDifficult: "addToDifficultButtonPushed",
+  pushedShowAnswerButton: "showAnswerButtonPushed"
+};
