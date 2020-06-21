e4fd9b17e806747e07afe323111ebccb34f0110f
Michael Fursa <bellamy9678@yahoo.com>
2020-06-21 14:16:54 +0300

    feat: add preHook

--- src/js/authorization/WelcomePage.js
+++ src/js/authorization/WelcomePage.js
@@ -1,27 +1,27 @@
-import DOMElementCreator from "../utils/DOMElementCreator";
+import DOMElementCreator from '../utils/DOMElementCreator';
 import TAGS from '../shared/Tags.json';
 
 export default function showWelcomePage(username) {
-	const app = document.querySelector(".app");
+	const app = document.querySelector('.app');
 	const newElem = new DOMElementCreator();
 
 	const settingsIcon = newElem.create({
 		elem: TAGS.SPAN,
-		classes: ["icon", "icon_settings"],
+		classes: ['icon', 'icon_settings'],
 	});
 
 	const settingsLinkText = newElem.create({
 		elem: TAGS.SPAN,
-		classes: "link-button__text",
-		child: ["Go to settings"],
+		classes: 'link-button__text',
+		child: ['Go to settings'],
 	});
 
 	const settingsLink = newElem.create({
 		elem: TAGS.A,
-		classes: "link-button",
+		classes: 'link-button',
 		attr: [
 			{
-				href: "#",
+				href: '#',
 			},
 		],
 		child: [settingsIcon, settingsLinkText],
@@ -29,47 +29,47 @@ export default function showWelcomePage(username) {
 
 	const title = newElem.create({
 		elem: TAGS.h1,
-		classes: "welcome__title",
+		classes: 'welcome__title',
 		child: [`Hello, ${username}! Let's start?`],
 	});
 
 	const buttons = newElem.create({
 		elem: TAGS.DIV,
-		classes: "welcome__buttons",
+		classes: 'welcome__buttons',
 		child: [settingsLink],
 	});
 
 	const content = newElem.create({
 		elem: TAGS.DIV,
-		classes: "welcome__content",
+		classes: 'welcome__content',
 		child: [title, buttons],
 	});
 
 	const image = newElem.create({
 		elem: TAGS.IMG,
-		classes: "welcome__image",
+		classes: 'welcome__image',
 		attr: [
 			{
-				src: "./assets/images/welcome.svg",
+				src: './assets/images/welcome.svg',
 			},
 			{
-				alt: "Settings",
+				alt: 'Settings',
 			},
 		],
 	});
 
 	const wrapper = newElem.create({
 		elem: TAGS.DIV,
-		classes: ["wrapper", "welcome__wrapper"],
+		classes: ['wrapper', 'welcome__wrapper'],
 		child: [content, image],
 	});
 
-  const page = newElem.create({
-    elem:  TAGS.DIV,
-    classes: 'welcome',
-    child: [wrapper],
-  });
-	
-  app.removeChild(app.firstChild);
-  app.append(page);
+	const page = newElem.create({
+		elem: TAGS.DIV,
+		classes: 'welcome',
+		child: [wrapper],
+	});
+
+	app.removeChild(app.firstChild);
+	app.append(page);
 }
