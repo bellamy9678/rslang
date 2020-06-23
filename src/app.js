25dc2d7106cd78ac6a8e276710e6d3b8b36417bf
Garza0 <konovalovandrey7@gmail.com>
2020-06-23 14:08:25 +0300

    fix: merge with master

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-import 'normalize.css';
-import { initEnglishPuzzle, startPuzzleGame } from './english_puzzle/js/main';
-
-initEnglishPuzzle();
-startPuzzleGame();
-=======
 import './sass/style.scss';
 import './js/authorization/CreateUser';
 import './js/authorization/StartPage';
->>>>>>> 3d61529718f9616b5236376edf3a003921e1e106
+import 'normalize.css';
+import { initEnglishPuzzle, startPuzzleGame } from './js/english_puzzle/main';
+
+initEnglishPuzzle();
+startPuzzleGame();
