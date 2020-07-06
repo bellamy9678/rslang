ed9df8373dfefe07c0b2207557a8258a4057d10d
chaba-chaba <lisaveta.chabotko@mail.ru>
2020-07-06 19:11:49 +0300

    feat: add increse and decrease of progress

--- src/js/words_service/constants.js
+++ src/js/words_service/constants.js
@@ -2,19 +2,24 @@ export const WORDS_MINIMUM_AMOUNT = 10;
 export const PART_OF_NEW_WORDS_IN_TOTAL = 0.5;
 
 export const DEFAULT_SETTINGS = {
-	progress : {
-		group : 0,
-		page : 0,
+	progress: {
+		group: 0,
+		page: 0,
 	},
 
-	useLearnedWords : true,
+	useLearnedWords: true,
 
-	incProgress : function incProgress(){
+	incProgress: function incProgress() {
 		this.progress.page += 1;
 		console.log("don't call me to many times");
 	},
 
-	saveParameters : function saveParameters() {
+	saveParameters: function saveParameters() {
 		console.log('saveParameters()');
 	},
+};
+
+export const PROGRESS = {
+	MAX: 4,
+	MIN: 0,
 };
