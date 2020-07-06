805afe67e03d8ffee085b76e33e7658948ec01e5
Michael Fursa <bellamy9678@yahoo.com>
2020-07-06 22:02:06 +0300

    feat: all services

--- src/js/words_service/Service.js
+++ src/js/words_service/Service.js
@@ -1,26 +1,48 @@
-
 import {
-	DEFAULT_SETTINGS,
-	WORDS_MINIMUM_AMOUNT,
 	PART_OF_NEW_WORDS_IN_TOTAL,
+	DEFAULT_GROUP,
+	DEFAULT_PAGE,
+	INTERVAL_PARAMS,
 } from './constants';
+import Settings from '../userSettings/Settings';
+import APIMethods from './APIMethods';
+import { CATEGORIES } from '../shared/Constants';
 
-const settings = DEFAULT_SETTINGS; // new SINGLETON_SETTINGS();
+const settings = new Settings();
+const api = new APIMethods();
 
 const Service = {};
 
-// 	getDifficultWordsDictionary() - получить из словаря сложные слова и возвращаем количество слов === settings.maxCards - settings.cardsShowed
-// 	getRepeatedWordsDictionary() - получить из словаря слова для повторения и возвращаем количество слов === settings.maxCards - settings.cardsShowed
-// 	SortByDate(wordsObjectsArray) - если у словаря этого нет и прилетит список всех слов каждой категории (сложные, для повтора и т.д.)
+function getShuffledArray(array) {
+	const output = array;
+	for (let i = array.length - 1; i > 0; i -= 1) {
+		const randomIndex = Math.floor(Math.random() * (i + 1));
+		[output[i], output[randomIndex]] = [array[randomIndex], array[i]];
+	}
+	return output;
+}
+
+function sortByShowTime(array) {
+	const filtered = array.filter((word) => {
+		return new Date(word.optional.learnDate) <= new Date();
+	});
+	return filtered;
+}
 
 Service.getGameWords = async function getGameWords() {
 	if (settings.useLearnedWords) {
-		const listLearnedWords = await []; // взять слова со школы, которые выученные
-		if (listLearnedWords.length > WORDS_MINIMUM_AMOUNT) {
-			return listLearnedWords;
-		}
+		const listLearnedWords = await api.getUserWordsByCategory(
+			CATEGORIES.ACTIVE
+		);
+		const filtered = listLearnedWords.filter((word) => {
+			return word.optional.progress === INTERVAL_PARAMS.MAX_PROGRESS_LEVEL;
+		});
+		return getShuffledArray(filtered);
 	}
-	const listOfNewWords = await Service.getNewWords();
+	const listOfNewWords = await Service.getGameSpecificWords(
+		DEFAULT_GROUP,
+		DEFAULT_PAGE
+	);
 	return listOfNewWords;
 };
 
@@ -28,63 +50,55 @@ Service.getGameSpecificWords = async function getGameSpecificWords(
 	level,
 	round
 ) {
-	const words = await (() => [])(level, round); // APIMethods getNewWordsArray(level, round);
-	return words;
+	const words = await api.getNewWordsArray(level, round);
+	return getShuffledArray(words);
 };
 
 Service.getNewWords = async function getNewWords() {
 	const total = settings.newWordsToShowAmount();
-	const userWords = await []; // получить слова юзера которые в хидден
+	const userWords = await api.getUserWordsByCategory(CATEGORIES.NEW);
 	if (userWords.length >= total) {
 		userWords.length = total;
 		return userWords;
 	}
-
-	const words = await (() => [])(settings.group, settings.page); // APIMethods getNewWordsArray(settings.group, settings.page);
+	const words = await api.getNewWordsArray(settings.group, settings.page);
 	settings.incProgress();
 	settings.saveParameters();
-	// сохранить все новые слова в словаре
-	return words;
+	api.saveWordsArray(words);
+	return getShuffledArray(words);
 };
 
 Service.getRandomWords = async function getRandomWords() {
 	const total = settings.cardsToShowAmount();
 	if (total === 0) return [];
 
-	// получить слова юзера которые в хидден
-	const newWords = await Service.getNewWords();
+	const hiddenWords = await api.getUserWordsByCategory(CATEGORIES.NEW);
+	if (hiddenWords.length < total) {
+		const newWords = await Service.getNewWords();
+		hiddenWords.concat(newWords);
+	}
 	const repeatedWords = await Service.getRepeatedWords();
-
 	const amountRepeatedWords = Math.round(total * PART_OF_NEW_WORDS_IN_TOTAL);
 	const amountNewWords = total - amountRepeatedWords;
 
-	const output = [];
-	for (let i = 0; i < amountRepeatedWords; i += 1) {
-		output.push(repeatedWords[i]);
-	}
-	for (let i = 0; i < amountNewWords; i += 1) {
-		output.push(newWords[i]);
-	}
-
-	return output.getShuffledArray();
+	const output = repeatedWords
+		.slice(null, amountRepeatedWords)
+		.concat(hiddenWords.slice(null, amountNewWords));
+	return getShuffledArray(output);
 };
 
 Service.getRepeatedWords = async function getRepeatedWords() {
-	const userWords = await []; // получить слова из апи Repeated
+	const userWords = await api.getUserWordsByCategory(CATEGORIES.NEW);
 	const total =
 		settings.cardsToShowAmount() >= userWords.length
 			? userWords.length
 			: settings.cardsToShowAmount();
-	const repeatedWords = [];
-	userWords.sortByShowTime();
-	for (let i = 0; i < total; i += 1) {
-		repeatedWords.push(userWords[i]);
-	}
-	return repeatedWords;
+	const repeatedWords = sortByShowTime(userWords);
+	return repeatedWords.slice(null, total);
 };
 
 Service.getDifficultWords = async function getDifficultWords() {
-	const userWords = await []; // получить слова из апи Difficult
+	const userWords = await api.getUserWordsByCategory(CATEGORIES.DIFFICULT);
 	const total =
 		settings.cardsToShowAmount() >= userWords.length
 			? userWords.length
--- src/js/words_service/constants.js
+++ src/js/words_service/constants.js
@@ -1,25 +1,9 @@
 export const WORDS_MINIMUM_AMOUNT = 10;
 export const PART_OF_NEW_WORDS_IN_TOTAL = 0.5;
+export const DEFAULT_GROUP = 0;
+export const DEFAULT_PAGE = 0;
 
-export const DEFAULT_SETTINGS = {
-	progress: {
-		group: 0,
-		page: 0,
-	},
-
-	useLearnedWords: true,
-
-	incProgress: function incProgress() {
-		this.progress.page += 1;
-		console.log("don't call me to many times");
-	},
-
-	saveParameters: function saveParameters() {
-		console.log('saveParameters()');
-	},
-};
-
-export const PROGRESS = {
-	MAX: 4,
-	MIN: 0,
-};
+export const INTERVAL_PARAMS = {
+	MAX_PROGRESS_LEVEL : 4,
+	MIN_PROGRESS_LEVEL : 0,
+}
