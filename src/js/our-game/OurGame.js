5106361236fe84a9f2c5787f7532ec98d4fa76f9
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-07-10 01:53:42 +0300

    feat: remove listeners

--- src/js/our-game/OurGame.js
+++ src/js/our-game/OurGame.js
@@ -16,7 +16,7 @@ import initStartPage from './StartPage';
 
 import Result from '../game_result/Result';
 
-import emptyString from './OurGameConsts';
+import { emptyString, finishText, statisticText, newGameText } from './OurGameConsts';
 
 async function getData(url) {
 	const response = await fetch(url);
@@ -60,15 +60,19 @@ function initGame() {
 		classes: ['game-score', 'none'],
 	});
 
+	const finishBtn = newElem.create({
+		elem: TAGS.BUTTON,
+		classes: ['start-btn', 'button', 'button_colored'],  
+		child: finishText,
+	});
+
 	const wrapper = newElem.create({
 		elem: TAGS.DIV,
 		classes: ['wrapper', 'ourgame__wrapper'],  
-		child: [initStartPage(), score, initMain()],
+		child: [initStartPage(), score, initMain(), finishBtn],
 	});
 
 	app.append(wrapper);
-
-
 
 
 	const startPage = document.querySelector('.start-page');
@@ -83,12 +87,16 @@ function initGame() {
 	const correctSound = new Audio('https://raw.githubusercontent.com/garza0/rslang-data/master/correct.mp3');
 	const wrongSound = new Audio('https://raw.githubusercontent.com/garza0/rslang-data/master/wrong.mp3');
 	const notGuessed = [];
-	points.textContent = 0;
+	const zero = 0;
+	points.textContent = zero;
 	let wordsNumber = 15;
 
+
 	function initGetWords() {
-		getWords(0,0).then(res => {
-			let words = res.slice(0, 15).map(item => renderWords(item));
+		const level = document.getElementById('level').value || 0;
+		const round = document.getElementById('round').value  || 0;
+		getWords(level, round).then(res => {
+			let words = res.slice(zero, wordsNumber).map(item => renderWords(item));
 			words = shuffle(words);
 			words.forEach(el => engWordsContainer.append(el));
 		
@@ -99,121 +107,155 @@ function initGame() {
 	}
 
 
-	
+	function GameHandlers() {
 
-	function startNewGame() {
-		gameResult.closeResultWindow();
-		startPage.classList.add('none');
-		points.classList.remove('none');
-		gameContainer.classList.add('none');
-		[...engWords].forEach(el => el.remove());
-		[...translationContainer].forEach(el => el.remove());
-		points.textContent = emptyString;
-		initGetWords();
-	}
+		this.statisticBtn = document.querySelector('.stat');
 
-	function resultBtnHandler() {
-		// создаю элемент button с помощью DOMElementCreator
-		const statisticBtn = newElem.create({
-			elem: TAGS.BUTTON,
-			classes: ['result__button', 'result__continue-btn'],
-			child: 'Statistic',
-		});
-
-		const newGameBtn = newElem.create({
-			elem: TAGS.BUTTON,
-			classes: ['result__button', 'result__continue-btn'],
-			child: 'New Game',
-		});
-
-
-		// добавляю eventListener на button
-		statisticBtn.addEventListener('click', () => {
-		// сначала закрываем модальное окно
+		this.startNewGame =  () => {
 			gameResult.closeResultWindow();
-			// remove all listeners
-		});
+			startPage.classList.remove('none');
+			points.classList.add('none');
+			gameContainer.classList.add('none');
+			[...engWords].forEach(el => el.remove());
+			[...translation].forEach(el => el.remove());
+			points.textContent = emptyString;
+			initGetWords();
+		};
 
-		newGameBtn.addEventListener('click', startNewGame);
+		this.removeAllListeners = () =>{
+			gameResult.closeResultWindow();
+			startBtn.removeEventListener('click', this.startHandler);
+			document.removeEventListener('click', this.gameHandler);
+			finishBtn.removeEventListener('click', this.resultBtnHandler);
+			this.statisticBtn.removeEventListener('click', this.removeAllListeners);
+		};
 
-		// отображаем результат
-		// массивы которые передаем в rightAnswers и wrongAnswers - это массивы обьектов со словами, которые мы получаем с backend
-		gameResult.showResult({
-			rightAnswers: ([...engWords].filter((item) => (!notGuessed.includes(item)))).map(item => new GetAnswers(item)),
-			wrongAnswers:  notGuessed.map(item => new GetAnswers(item)),
-			points: points.textContent,
-			buttons: [newGameBtn, statisticBtn],
-		});
-	}
-
-	function compareWords() {
-		const word = document.querySelector('.chosen');
-		const transl = document.querySelector('.active');
-		
-		if (word !== null && transl !== null) {
-			if( word.textContent === transl.dataset.word) {
-				word.classList.remove('chosen');
-				transl.classList.remove('active');
-				transl.classList.add('invisible');
-				word.classList.add('invisible');
-				correctSound.play();
-				wordsNumber -= 1;
-				if( !word.classList.contains('yellow')) {
-					points.textContent = +(points.textContent) + 100;
-				}
-				if (wordsNumber === 0) {
-					resultBtnHandler();
-					console.log('game-over');
-				}
+		this.resultBtnHandler = () => {
 			
-			} else {
-				word.classList.remove('chosen');
-				transl.classList.remove('active');
-				word.classList.add('yellow');
-				notGuessed.push(word);
-				wrongSound.play();			
-			}
-		}	
-	}
-
-
-	function gameHandler(event) {
-		if (event.target.classList.contains('word')) {
-			if(event.target.classList.contains('chosen')) {
-				event.target.classList.toggle('chosen');
-			} else { 
-				[...engWords].forEach( el => {
-					el.classList.remove('chosen');
-					event.target.classList.add('chosen');		
-				});
-				compareWords();
-			}		
-		}
+			const statisticBtn = newElem.create({
+				elem: TAGS.BUTTON,
+				classes: ['result__button', 'result__continue-btn', 'stat'],
+				child: statisticText,
+			});
 	
-		if (event.target.classList.contains('word-translation')) {
-			if (event.target.classList.contains('active')) {
-				event.target.classList.toggle('active');
-				// console.log('la');
-			} else { 
-				[...translation].forEach( el => {
-					el.classList.remove('active');
-					event.target.classList.add('active');
+			const newGameBtn = newElem.create({
+				elem: TAGS.BUTTON,
+				classes: ['result__button', 'result__continue-btn', 'new-btn'],
+				child: newGameText,
+			});
+	
+	
+			statisticBtn.addEventListener('click', this.removeAllListeners);
+	
+			newGameBtn.addEventListener('click', this.startNewGame);
+	
+			gameResult.showResult({
+				rightAnswers: ([...engWords].filter((item) => (!notGuessed.includes(item)))).map(item => new GetAnswers(item)),
+				wrongAnswers:  notGuessed.map(item => new GetAnswers(item)),
+				points: points.textContent,
+				buttons: [newGameBtn, statisticBtn],
+			});
+		};
+
+		this.compareWords = () =>{
+			const word = document.querySelector('.chosen');
+			const transl = document.querySelector('.active');
 			
-				});
+			if (word !== null && transl !== null) {
+				if( word.textContent === transl.dataset.word) {
+					word.classList.remove('chosen');
+					transl.classList.remove('active');
+					transl.classList.add('invisible');
+					word.classList.add('invisible');
+					correctSound.play();
+					wordsNumber -= 1;
+					if( !word.classList.contains('yellow')) {
+						points.textContent = +(points.textContent) + 100;
+					}
+					if (wordsNumber === zero) {
+						this.resultBtnHandler();
+					}
+				
+				} else {
+					word.classList.remove('chosen');
+					transl.classList.remove('active');
+					word.classList.add('yellow');
+					notGuessed.push(word);
+					wrongSound.play();			
+				}
+			}	
+		};
+	
+	
+		this.gameHandler = (event) => {
+			if (event.target.classList.contains('word')) {
+				if(event.target.classList.contains('chosen')) {
+					event.target.classList.toggle('chosen');
+				} else { 
+					[...engWords].forEach( el => {
+						el.classList.remove('chosen');
+						event.target.classList.add('chosen');		
+					});
+					this.compareWords();
+				}		
 			}
-			compareWords();
-		}
+		
+			if (event.target.classList.contains('word-translation')) {
+				if (event.target.classList.contains('active')) {
+					event.target.classList.toggle('active');
+				} else { 
+					[...translation].forEach( el => {
+						el.classList.remove('active');
+						event.target.classList.add('active');				
+					});
+				}
+				this.compareWords();
+			}	
+		};
 
+
+		this.gameHandler = (event) => {
+			if (event.target.classList.contains('word')) {
+				if(event.target.classList.contains('chosen')) {
+					event.target.classList.toggle('chosen');
+				} else { 
+					[...engWords].forEach( el => {
+						el.classList.remove('chosen');
+						event.target.classList.add('chosen');		
+					});
+					this.compareWords();
+				}		
+			}
+		
+			if (event.target.classList.contains('word-translation')) {
+				if (event.target.classList.contains('active')) {
+					event.target.classList.toggle('active');
+				} else { 
+					[...translation].forEach( el => {
+						el.classList.remove('active');
+						event.target.classList.add('active');				
+					});
+				}
+				this.compareWords();
+			}	
+		};
+
+		this.startHandler = () => {
+			startPage.classList.add('none');
+			points.classList.remove('none');
+			gameContainer.classList.remove('none');
+			document.addEventListener('click', this.gameHandler);
+			finishBtn.addEventListener('click', this.resultBtnHandler);
+			initGetWords();
+		};
+
+		this.addAllListeners = () => {
+			startBtn.addEventListener('click', this.startHandler);
+		};
 	}
 
-	function startHandler() {
-		startPage.classList.add('none');
-		points.classList.remove('none');
-		gameContainer.classList.remove('none');
-		document.addEventListener('click', gameHandler);
-		initGetWords();
-	}
-	startBtn.addEventListener('click', startHandler);	
+	const ourGame = new GameHandlers();
+	ourGame.addAllListeners();
 }
 
 initGame();
