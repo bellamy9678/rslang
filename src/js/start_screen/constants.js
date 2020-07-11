8bf2586f7a9f20161252d776a0252cfbe1094556
Andrey <konovalovandrey7@gmail.com>
2020-07-11 14:30:17 +0300

    fix: change number of rounds

--- src/js/start_screen/constants.js
+++ src/js/start_screen/constants.js
@@ -3,7 +3,7 @@ export const REPEAT_WORDS = 'Повтор слов';
 export const RSLANG_DATABASE_WORDS = 'Слова RSLang';
 export const MAIN_CONTAINER = document.querySelector('.app');
 export const LEVELS = 6;
-export const ROUNDS = 10;
+export const ROUNDS = 30;
 export const LEVEL_TEXT = 'Уровень';
 export const ROUND_TEXT = 'Раунд';
 
