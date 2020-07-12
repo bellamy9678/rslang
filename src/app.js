19c96356efea1e51b8813164c10a48314dbdb3d6
leqsar <47753538+leqsar@users.noreply.github.com>
2020-07-12 19:47:41 +0300

    refactor: remove coments

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 import './sass/style.scss';
 
-// import './js/authorization/NewUser';
-// import './js/authorization/StartPage';
-// import './js/authorization/WindowOnload';
-// import Settings from './js/settings/Settings';
-// // import training from './js/cards/Training';
-// import './js/dictionary/CategoryView';
-// import subscribeToEvents from './js/events/EventCatcher';
-//
-// async function initial() {
-// 	let settings = new Settings();
-// 	await Settings.init();
-// 	settings = await Settings.getInstance();
-// 	// await training();
-// 	return settings;
-// }
-//
-// initial();
-// subscribeToEvents();
-// subscribeToEvents();
-import savannah from './js/savannah/savannah';
+import './js/authorization/NewUser';
+import './js/authorization/StartPage';
+import './js/authorization/WindowOnload';
+import Settings from './js/settings/Settings';
+// import training from './js/cards/Training';
+import './js/dictionary/CategoryView';
+import subscribeToEvents from './js/events/EventCatcher';
 
-savannah();
+async function initial() {
+	let settings = new Settings();
+	await Settings.init();
+	settings = await Settings.getInstance();
+	// await training();
+	return settings;
+}
+
+initial();
+subscribeToEvents();
+subscribeToEvents();
