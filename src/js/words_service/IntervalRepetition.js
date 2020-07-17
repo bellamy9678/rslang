808e4ba233a69bc5a403682ca1cd5bb2391356b8
chaba-chaba <lisaveta.chabotko@mail.ru>
2020-07-17 20:51:27 +0300

    fix: fix bug with updating word progress

--- src/js/words_service/IntervalRepetition.js
+++ src/js/words_service/IntervalRepetition.js
@@ -12,8 +12,10 @@ export default class IntervalRepetition {
 	}
 
 	async increaseProgress() {
-		if (this.bestResult <= INTERVAL_PARAMS.MAX_PROGRESS_LEVEL) {
+		if (this.optional.bestResult <= INTERVAL_PARAMS.MAX_PROGRESS_LEVEL) {
 			this.optional.progress = this.optional.bestResult;
+		} else {
+			this.optional.progress = INTERVAL_PARAMS.MAX_PROGRESS_LEVEL;
 		}
 		await APIMethods.updateUserWord(this.wordId, {
 			optional: this.optional
